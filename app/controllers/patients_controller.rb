e1f8474cec3a233239a50da6dd6a6a9169ffb811
joalorro <joalorro@gmail.com>
2018-10-13 15:55:01 -0400

    added create route functionality for patients controller

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,13 +1,31 @@
 class PatientsController < ApplicationController
 	before_action :set_patient, only: [:show,:update]
 
-	def show 
+	def index 
+		@patients = Patient.all 
+		render json: @patients, status: 200
+	end 
 
+	def show 
 		render json: @patient, status: 200
 	end 
 
 	def create 
-
+		@patient = Patient.new patient_params
+		if @patient.save 
+			render json: @patient, status: 200
+		else 
+			byebug
+		end 
+		# respond_to do |format|
+		# 	if @patient.save
+		# 		format.html { redirect_to @patient, notice: 'patient was successfully created.' }
+		# 		format.json { render :show, status: :created, location: @patient }
+		# 	else
+		# 		format.html { render :new }
+		# 		format.json { render json: @patient.errors, status: :unprocessable_entity }
+		# 	end
+		# end 
 	end 
 
 	def update 
@@ -17,7 +35,7 @@ class PatientsController < ApplicationController
 	private 
 
 		def patient_params 
-
+			params.require(:patient).permit(:first_name,:last_name,:email,:password)
 		end 
 
 		def set_patient 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	resources :therapists, only: [:show,:create,:update]
-	resources :patients, only: [:show,:create,:destroy,:update]
+	resources :patients, only: [:index,:show,:create,:destroy,:update]
 	resources :programs, only: [:show,:create,:destroy,:update]
 	resources :exercises, except: [:index]
 	resources :comments, except: [:index]
